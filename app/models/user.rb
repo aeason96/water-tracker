fb7dc9cc97aa25e4c7890fa463ed5801edad9b1b
Nik Weinmeister <nik.weinmeister@excella.com>
2018-08-24 17:24:02 -0400

    create user and secure password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ApplicationRecord
-    validates :username, presence: true, uniqueness: true 
+    has_secure_password
     has_many :drinks, dependent: :destroy
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_21_175508) do
+ActiveRecord::Schema.define(version: 2018_08_21_173911) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,10 +33,9 @@ ActiveRecord::Schema.define(version: 2018_08_21_175508) do
 
   create_table "users", force: :cascade do |t|
     t.string "email"
-    t.string "password"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "username"
   end
 
   add_foreign_key "drinks", "users"
